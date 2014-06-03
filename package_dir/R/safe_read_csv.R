bd9901ef88d8839436a2f7d943ac27846a520d9b
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2014-06-03 04:41:02 +0000

    Capitalization is one of those things NOBODY seems to be able to be consistent with, if it is not meaningful in the original names, save yourself some trouble and drop it right after the read.csv stage.

--- package_dir/R/safe_read_csv.R
+++ package_dir/R/safe_read_csv.R
@@ -1,11 +1,13 @@
-safe_read_csv <- function( file, instructions=NULL) {
+safe_read_csv <- function( file, instructions=NULL, drop_uppercase=FALSE) {
 	e <- new.env()
 	if (!is.null(instructions)) {
 		source(instructions, local=e, echo=FALSE)
 	}
 	data <- read.csv(file=file, check.names=FALSE, 
 									 colClasses='character', stringsAsFactors=FALSE)
-	
+
+	if(drop_uppercase) colnames(data) <- tolower(colnames(data))
+
 	if(exists(x="column_map", where=e, inherits=FALSE)) {
 		names(data) <- map(x=names(data), map=as.list(e[['column_map']]))
 	}
